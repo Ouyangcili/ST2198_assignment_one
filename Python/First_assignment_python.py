6713e76ec8bd3249d263c4b0ae3f256711bd3d33
OuyangCili <118220897+Ouyangcili@users.noreply.github.com>
2022-11-22 18:30:22 +0000

    Add files via upload

---
+++ Python/First_assignment_python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 21:23:23 2022
+
+@author: oycl
+"""
+number = 1
+while number <= 10:
+    print (number)
+    number = number +1
+    
+
+
---
+++ R/First_assignment_R.R
@@ -0,0 +1,8 @@
+# 1st way of print integer 1-10
+1:10
+
+# 2nd way of print integer 1-10
+seq (1, 10)
+
+# 3rd way of print integer 1-10
+seq (from = 1, to = 10)
